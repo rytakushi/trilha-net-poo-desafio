aa54eb07853fb756a3e7ddff5756339c99f362ac
Hiko <rytakushiry@gmail.com>
2025-06-02 18:01:58 -0300

    Desafio celular concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,9 +1,12 @@
 namespace DesafioPOO.Models
 {
-    public abstract class Smartphone
+        public abstract class Smartphone
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        public int Memoria { get; set; }
 
         public Smartphone(string numero)
         {
@@ -14,11 +17,13 @@ namespace DesafioPOO.Models
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
+            Console.WriteLine($"{Numero} está realizando uma ligação.");
         }
 
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine($"{Numero} está recebendo uma ligação.");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
+   // Criando instâncias dos smartphones
+    Smartphone nokia = new Nokia ("1111-2222");
+    Smartphone iphone = new Iphone("3333-4444");
+
+    // Testando funcionalidades
+    Console.WriteLine("Testando Nokia:");
+    nokia.Ligar();
+    nokia.ReceberLigacao();
+    nokia.InstalarAplicativo("WhatsApp");
+
+    Console.WriteLine("\nTestando iPhone:");
+    iphone.Ligar();
+    iphone.ReceberLigacao();
+    iphone.InstalarAplicativo("Instagram");
+
+    Console.WriteLine("\nTeste concluído!");
+
